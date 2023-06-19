64d678d90a235d4a052e39f80f7d4288d4c9c9ab
mattjonesc <matt.jones.c@gmail.com>
2023-06-19 07:17:10 -0400

    working on both dev and prod

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,7 +4,7 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
-    fetch(`/sentiment?variable=${encodeURIComponent(formText)}`)
+    fetch(`http://localhost:3000/sentiment?variable=${encodeURIComponent(formText)}`)
     .then(res => {
         return res.json();
     })
